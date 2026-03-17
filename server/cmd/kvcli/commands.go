dff24227cd2aefc836adc0ad145acdebbb934b1f
Aditya Rana <adityarana181105@gmail.com>
2026-03-17 13:34:24 +0530

    adding some commands in kvcli repl

--- server/cmd/kvcli/commands.go
+++ server/cmd/kvcli/commands.go
@@ -29,35 +29,65 @@ var getCmd = &cobra.Command{
 		}
 		defer conn.Close()
 
-		proCmd, err := buildCommand([]string{"GET", args[0]})
+		if err := runCommand(conn, []string{"GET", args[0]}); err != nil {
+			fmt.Println("Error:", err)
+		}
+	},
+}
+
+var setCmd = &cobra.Command{
+	Use:   "set <key> <value> [ttl]",
+	Short: "Set a value by key",
+	Args:  cobra.RangeArgs(2, 3),
+	Run: func(cmd *cobra.Command, args []string) {
+		host, _ := cmd.Root().PersistentFlags().GetString("host")
+		port, _ := cmd.Root().PersistentFlags().GetInt("port")
+
+		conn, err := connect(host, port)
 		if err != nil {
-			fmt.Println("Build command error:", err)
+			fmt.Println("Connection error:", err)
 			return
 		}
+		defer conn.Close()
 
-		err = protocol.WriteCommand(conn, proCmd)
-		if err != nil {
-			fmt.Println("Write command error:", err)
-			return
+		tokens := []string{"SET", args[0], args[1]}
+		if len(args) == 3 {
+			tokens = append(tokens, args[2])
 		}
 
-		resp, err := protocol.ReadResponse(conn)
-		if err != nil {
-			fmt.Println("Read response error:", err)
-			return
+		if err := runCommand(conn, tokens); err != nil {
+			fmt.Println("Error:", err)
 		}
-
-		printResponse(resp)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-	// rootCmd.AddCommand(setCmd)
+	rootCmd.AddCommand(setCmd)
 	// rootCmd.AddCommand(delCmd)
 	// rootCmd.AddCommand(incrCmd)
 	// rootCmd.AddCommand(ttlCmd)
 	// rootCmd.AddCommand(keysCmd)
 	// rootCmd.AddCommand(expireCmd)
 	// rootCmd.AddCommand(pingCmd)
+}
+
+func runCommand(conn net.Conn, tokens []string) error {
+	proCmd, err := buildCommand(tokens)
+	if err != nil {
+		return err
+	}
+
+	err = protocol.WriteCommand(conn, proCmd)
+	if err != nil {
+		return err
+	}
+
+	resp, err := protocol.ReadResponse(conn)
+	if err != nil {
+		return err
+	}
+
+	printResponse(resp)
+	return nil
 }
