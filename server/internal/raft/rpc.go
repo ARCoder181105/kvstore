7f5f1e2639341bac502edafefdc53f900f9ca8fe
Aditya Rana <adityarana181105@gmail.com>
2026-04-05 07:02:20 +0530

    request vote complete

--- server/internal/raft/rpc.go
+++ server/internal/raft/rpc.go
@@ -1,2 +1,53 @@
 package raft
 
+type RequestVoteArgs struct {
+	Term         uint64
+	CandidateID  NodeID
+	LastLogIndex uint64
+	LastLogTerm  uint64
+}
+
+type RequestVoteReply struct {
+	Term        uint64
+	VoteGranted bool
+}
+
+func (r *RaftNode) RequestVote(args RequestVoteArgs) RequestVoteReply {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if args.Term > r.currentTerm {
+		r.currentTerm = args.Term
+		r.votedFor = NoVote
+		r.state = Follower
+	}
+
+	if args.Term < r.currentTerm {
+		return RequestVoteReply{
+			Term:        r.currentTerm,
+			VoteGranted: false,
+		}
+	}
+
+	if r.votedFor != NoVote && r.votedFor != args.CandidateID {
+		return RequestVoteReply{
+			Term:        r.currentTerm,
+			VoteGranted: false,
+		}
+	}
+
+	lastTerm := r.getLastTerm()
+	lastIndex := r.getLastIndex()
+	if lastTerm > args.LastLogTerm || (lastTerm == args.LastLogTerm && lastIndex > args.LastLogIndex) {
+		return RequestVoteReply{
+			Term:        r.currentTerm,
+			VoteGranted: false,
+		}
+	}
+
+	r.votedFor = args.CandidateID
+	return RequestVoteReply{
+		Term:        r.currentTerm,
+		VoteGranted: true,
+	}
+}
